c2f33585b2cdd771751cb7a7da00112b45c1cd87
Jack Smith <jasmith@wausaucoated.com>
2013-08-11 09:04:41 -0500

    split admin ui js into site.js

---
+++ LinkToolbar/Scripts/site.js
@@ -0,0 +1,60 @@
+﻿$(document).ready(function () {
+    var transport = {
+        read: {
+            url: "/links",
+            dataType: "json"
+        }
+    };
+
+    var menuSource = new kendo.data.DataSource({
+        transport: transport,
+        schema: {
+            data: function (data) {
+                var parse = function (links) {
+                    return $.map(links, function (link) {
+                        return {
+                            text: link.Name,
+                            imageUrl: link.ImageSrc,
+                            items: parse(link.Links)
+                        };
+                    });
+                };
+
+                return parse(data.Links);
+            }
+        }
+    });
+
+    var treeSource = new kendo.data.HierarchicalDataSource({
+        transport: transport,
+        schema: {
+            data: function (data) {
+                var parse = function (links) {
+                    return $.map(links, function (link) {
+                        return {
+                            text: link.Name,
+                            imageUrl: link.ImageSrc,
+                            items: parse(link.Links)
+                        };
+                    });
+                };
+
+                return parse(data.Links);
+            }
+        }
+    });
+
+    $("#menu").kendoMenu({
+        datasource: source,
+        autobind: false,
+        closeOnClick: false,
+        select: function (e) {
+            // handle event
+        }
+    });
+
+    $("#tree").kendoTreeview({
+        datasource: treeSource,
+        autobind: false
+    });
+});
