22589a846b337f2a25f8f1ebbcc7b9867d393b98
Sean Epping <sean.epping@gmail.com>
2013-08-12 23:17:09 -0500

    removed vs template text from home index action

--- LinkToolbar/Controllers/HomeController.cs
+++ LinkToolbar/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace LinkToolbar.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
     }
